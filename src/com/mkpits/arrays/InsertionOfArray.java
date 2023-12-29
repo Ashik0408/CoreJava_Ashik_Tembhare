d6bae1de05ae84fbd77395bf3ae1e0139dd3c269
Ashik0408 <ashikt128@gmail.com>
2023-12-29 20:04:38 +0530

    Abstraction Program

---
+++ src/com/mkpits/arrays/InsertionOfArray.java
@@ -0,0 +1,13 @@
+package com.mkpits.arrays;
+
+public class InsertionOfArray 
+{
+
+	public static void main(String[] args) 
+	{
+		int []array= {1,2,3,4,5,6};
+		System.out.println(array.length);
+
+	}
+
+}
